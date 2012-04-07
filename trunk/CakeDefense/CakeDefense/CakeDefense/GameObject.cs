6792ff79eb7cc7b9076cc762b8a1c05f126d3d35
tjs7664@g.rit.edu <tjs7664@g.rit.edu@7a81e0e6-3be7-dba3-acd0-906c2c96c0bd>
2012-04-07 01:29:56 +0000

    Well, pathfinding works (for a single path choice), though my move code does not (properly). I am committing this encase anyone needs what I've done so far. ~Torey

--- trunk/CakeDefense/CakeDefense/CakeDefense/GameObject.cs
+++ trunk/CakeDefense/CakeDefense/CakeDefense/GameObject.cs
@@ -20,10 +20,10 @@ namespace CakeDefense
     {
         protected int strtHlth, currHealth, dmgDn, speed; // Max health, current health, damage that the object does, either speed of enemy or speed of bullet
         protected bool isActive; // If the object is currently active
-        protected Rectangle location; // The current location and size of the object
+        protected Rectangle rectangle; // The current location and size of the object
         protected SpriteBatch sprBtch; // Objects picture
         protected Color color; // Color shows the type of enemy or tower
-        protected Texture pic; // Picture
+        protected Texture2D pic; // Picture
 
         #region Constructor
         /// <param name="sh">Max Health</param>
@@ -32,14 +32,14 @@ namespace CakeDefense
         /// <param name="spB">Objects Picture</param>
         /// <param name="c">Color</param>
         /// <param name="t">Texture (Picture)</param>
-        public GameObject(int sh, int dd, int sp, int x, int y, int w, int h, SpriteBatch spB, Color c, Texture t)
+        public GameObject(int sh, int dd, int sp, int x, int y, int w, int h, SpriteBatch spB, Color c, Texture2D t)
         {
             strtHlth = sh;
             currHealth = sh;
             dmgDn = dd;
             isActive = true;
             speed = sp;
-            location = new Rectangle(x, y, w, h);
+            rectangle = new Rectangle(x, y, w, h);
             sprBtch = spB;
             color = c;
             pic = t;
@@ -56,19 +56,67 @@ namespace CakeDefense
 
         public Rectangle Rectangle
         {
-            get { return location; }
+            get { return rectangle; }
+        }
+
+        public Vector2 Vector
+        {
+            get { return new Vector2(rectangle.X, rectangle.Y); }
+
+            set { rectangle.X = (int)value.X; rectangle.Y = (int)value.Y; }
+        }
+
+        public Texture2D Texture
+        {
+            get { return pic; }
+
+            set { pic = value; }
+        }
+
+        public Color Color
+        {
+            get { return color; }
+
+            set { color = value; }
+        }
+
+        public int X
+        {
+            get { return rectangle.X; }
+
+            set { rectangle.X = value; }
+        }
+
+        public int Y
+        {
+            get { return rectangle.Y; }
+
+            set { rectangle.Y = value; }
+        }
+
+        public int Width
+        {
+            get { return rectangle.Width; }
+
+            set { rectangle.Width = value; }
+        }
+
+        public int Height
+        {
+            get { return rectangle.Height; }
+
+            set { rectangle.Height = value; }
+        }
+
+        public Vector2 Center
+        {
+            get { return new Vector2(rectangle.X + Width / 2, rectangle.Y + Height / 2); }
+
+            set { rectangle.X = (int)value.X - (Width / 2); rectangle.Y = (int)value.Y - (Height / 2); }
         }
         #endregion Properties
 
         #region Other Methods
-        public void Move(int[,] tileArray)
-        {
-            //check current tile
-            //if a valid path tile, move along it towards the next
-            //if(tileArray[(int)location.X / 40, (int)location.Y / 40] == 1)
-            //round up?
-                location.X++;
-        }
 
         public void TakeDamage(int damage)
         {
@@ -77,5 +125,13 @@ namespace CakeDefense
                 isActive = false;
         }
         #endregion Other Methods
+
+        #region Draw
+        public void Draw()
+        {
+            if(isActive)
+                sprBtch.Draw(Texture, Rectangle, Color);
+        }
+        #endregion Draw
     }
 }
---
+++ trunk/CakeDefense/CakeDefense/CakeDefense/Tile - Map/Map.cs
@@ -0,0 +1,193 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Diagnostics;
+using System.IO;
+#endregion using
+
+namespace CakeDefense
+{
+    class Map
+    {
+        #region Attributes
+        //this can be scaled however is needed. right now its set to 18 x 18 tiles with a border of 2
+        //not very hard to then draw a path from this based on the grid
+        //actual tiles can be implemented just as easily
+        private Tile[,] tiles;
+        private Point size;
+        SpriteBatch sprite;
+        #endregion Attributes
+
+        #region Constructor
+        public Map(int tilesWide, int tilesHigh, SpriteBatch spriteBatch)
+        {
+            sprite = spriteBatch;
+            size = new Point(tilesWide, tilesHigh);
+            //create list for use in loop to change specific tiles
+            //32 wide 18 high
+            //TODO: add weights to path
+
+            int [,] tileMap = new int [,]{
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
+                {1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,0,0,0,0},
+                {0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,1,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0}
+            };
+
+            tiles = new Tile[tileMap.GetUpperBound(1) + 1, tileMap.GetUpperBound(0) + 1];
+            for (int i = 0; i <= tileMap.GetUpperBound(1); i++ )
+            {
+                for (int j = 0; j <= tileMap.GetUpperBound(0); j++)
+                {
+                    if (tileMap[j, i] == 0)
+                        tiles[i, j] = new Tile_Tower(i * Var.TILE_SIZE, j * Var.TILE_SIZE, Var.TILE_SIZE, Var.TILE_SIZE, new Point(i,j));
+                    else if (tileMap[j, i] == 1)
+                        tiles[i, j] = new Tile_Path(i * Var.TILE_SIZE, j * Var.TILE_SIZE, Var.TILE_SIZE, Var.TILE_SIZE, new Point(i, j));
+                }
+            }
+            AssignTileNeighbors();
+        }
+        #endregion region Constructor
+
+        #region Properties
+
+        public Tile[,] Tiles
+        {
+            get { return tiles; }
+        }
+
+        #endregion Properties
+
+        #region Methods
+        public void AssignTileNeighbors()
+        {
+            // Now for each of the search nodes, we will
+
+            // connect it to each of its neighbours.
+
+            for (int x = 0; x < size.X; x++)
+            {
+
+                for (int y = 0; y < size.Y; y++)
+                {
+
+                    // An array of all of the possible neighbors this 
+
+                    // node could have. (We will ignore diagonals for now.)
+
+                    Point[] neighbors = new Point[]
+
+                    {
+
+                        new Point (x, y - 1), // The node above the current node
+
+                        new Point (x, y + 1), // The node below the current node.
+
+                        new Point (x - 1, y), // The node left of the current node.
+
+                        new Point (x + 1, y), // The node right of the current node
+
+                    };
+
+
+
+                    // We loop through each of the possible neighbors
+
+                    for (int i = 0; i < neighbors.Length; i++)
+                    {
+
+                        Point position = neighbors[i];
+
+
+
+                        // We need to make sure this neighbour is part of the level.
+
+                        if (position.X < 0 || position.X >= size.X ||
+
+                            position.Y < 0 || position.Y >= size.Y)
+                        {
+
+                            continue;
+
+                        }
+
+
+                        // Store a reference to the neighbor.
+                        tiles[x, y].Neighbors[i] = tiles[position.X, position.Y];
+
+                    }
+
+                }
+
+            }
+        }
+
+        public List<Tile> FindPath(Tile startNode, Tile endNode, int pathNum)
+        {
+            List<Tile> path = new List<Tile>();
+            Tile lastTile = startNode;
+            Tile currentTile = startNode;
+            path.Add(currentTile);
+            bool[,] visited = new bool[tiles.GetUpperBound(0) + 1, tiles.GetUpperBound(1) + 1];
+            visited[currentTile.TileNum.X, currentTile.TileNum.Y] = true;
+            bool movesLeft = true;
+
+            while (currentTile != endNode && movesLeft == true)
+            {
+                movesLeft = false;
+                foreach (Tile neighbor in currentTile.Neighbors)
+                {
+                    if (neighbor is Tile_Path && visited[neighbor.TileNum.X, neighbor.TileNum.Y] == false)
+                    {
+                        movesLeft = true;
+                        visited[neighbor.TileNum.X, neighbor.TileNum.Y] = true;
+                        lastTile = currentTile;
+                        currentTile = neighbor;
+                        path.Add(currentTile);
+                        break;
+                    }
+                }
+            }
+
+            return path;
+        }
+        #endregion Methods
+
+        #region Draw
+        public void DrawMap(Texture2D tex, SpriteFont font)
+        {
+            foreach (Tile tile in tiles)
+            {
+                if (tile is Tile_Tower)
+                    sprite.Draw(tex, tile.Rectangle, Color.White);
+                else if (tile is Tile_Path)
+                    sprite.Draw(tex, tile.Rectangle, Color.Black);
+                sprite.DrawString(font, "x:" + tile.TileNum.X.ToString("00") + "\ny:" + tile.TileNum.Y.ToString("00"), tile.Vector, Color.Green);
+            }
+        }
+        #endregion Draw
+    }
+}
